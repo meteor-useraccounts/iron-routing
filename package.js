f67fd8c65b12751124855a67cfb8d829ddd1226c
Luca Mussi <ing.luca.mussi@gmail.com>
2015-08-05 01:32:23 +0200

    first attempt to take iron router related stuff out of core

---
+++ package.js
@@ -0,0 +1,41 @@
+// Package metadata for Meteor.js web platform (https://www.meteor.com/)
+// This file is defined within the Meteor documentation at
+//
+//   http://docs.meteor.com/#/full/packagejs
+//
+// and it is needed to define a Meteor package
+'use strict';
+
+Package.describe({
+  name: 'useraccounts:iron-routing',
+  summary: 'UserAccounts package providing routes configuration capability via iron:router.',
+  version: '1.11.1',
+  git: 'https://github.com/meteor-useraccounts/iron-routing.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.0.3');
+
+  api.use([
+    'check',
+    'iron:router',
+    'underscore',
+    'useraccounts:core',
+  ], ['client', 'server']);
+
+  api.imply([
+    'useraccounts:core@1.11.1',
+    'iron:router@1.0.7',
+  ], ['client', 'server']);
+
+  api.addFiles([
+    'lib/core.js',
+    'lib/server.js',
+  ], ['server']);
+
+  api.addFiles([
+    'lib/core.js',
+    'lib/client.js',
+    'lib/templates_helpers/at_input.js',
+  ], ['client']);
+});
