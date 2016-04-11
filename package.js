eb4d6a1a03f35ffa49668826087fe4bce27ecb91
Luca Mussi <ing.luca.mussi@gmail.com>
2016-04-11 23:31:32 +0200

    patch release - Bump to version 1.14.2

--- package.js
+++ package.js
@@ -9,7 +9,7 @@
 Package.describe({
   name: 'useraccounts:iron-routing',
   summary: 'UserAccounts package providing routes configuration capability via iron:router.',
-  version: '1.14.1',
+  version: '1.14.2',
   git: 'https://github.com/meteor-useraccounts/iron-routing.git'
 });
 
@@ -24,7 +24,7 @@ Package.onUse(function(api) {
   ], ['client', 'server']);
 
   api.imply([
-    'useraccounts:core@1.14.1',
+    'useraccounts:core@1.14.2',
     'iron:router@1.0.9',
   ], ['client', 'server']);
 
